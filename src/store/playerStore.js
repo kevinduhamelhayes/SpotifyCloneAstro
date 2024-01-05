60438daee8555b61d9633ef12191de6176f31e3b
kevinduhamelhayes <kevinduhamelh@gmail.com>
2024-01-05 00:05:49 -0300

    add music

--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -2,9 +2,9 @@ import { create } from "zustand";
 
 export const usePlayerStore = create((set) => ({
   isPlaying: false,
-  currentMusic: {playlist: null, songs: [], song: null},
+  currentMusic: { playlist: null, song: null, songs: [] },
   volume: 1,
-  setVolume: (volume) => set({volume}),
-  setIsPlaying: (isPlaying) => set({isPlaying}),
-  setCurrentMusic: (currentMusic) => set({currentMusic}),
-}));
+  setVolume: (volume) => set({ volume }),
+  setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setCurrentMusic: (currentMusic) => set({ currentMusic }),
+}))
